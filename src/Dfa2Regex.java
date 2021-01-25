f3e6b645cef401d2b13f04b0443e979993920e9b
çağırkan <halil.cagirkan@ceng.deu.edu.tr>
2021-01-25 18:35:01 +0300

    Read completed

---
+++ src/Dfa2Regex.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Dfa2Regex {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter dfa file name (e.g. DFA1.txt) :");
+        String fileName = sc.nextLine();
+        ArrayList<String> lines = readFromFile(fileName);
+    }
+
+    public static ArrayList<String> readFromFile(String fileName) throws Exception {
+        ArrayList<String> lines = new ArrayList<>();
+        URL path = Dfa2Regex.class.getResource(fileName);
+        File file = new File(path.getFile());
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        while ((st = br.readLine()) != null)
+            lines.add(st);
+
+        return lines;
+    }
+}
